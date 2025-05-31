fe784938510fa86e21cc2d76f59cc722384241c0
daniel pogodin <dncar13@gmail.com>
2025-05-31 14:34:37 +0300

    Add temporary dummy firebase.ts for build compatibility

---
+++ src/lib/firebase.ts
@@ -0,0 +1,12 @@
+export const signInWithGoogle = async () => ({ user: null, error: new Error("Not implemented") });
+export const loginWithEmailAndPassword = async () => ({ user: null, error: new Error("Not implemented") });
+export const registerWithEmailAndPassword = async () => ({ user: null, error: new Error("Not implemented") });
+
+export const logoutUser = async () => ({ success: false, error: new Error("Not implemented") });
+
+export const checkFirebaseConnection = async () => false;
+
+export const auth = null;
+export const db = null;
+export const onAuthStateChanged = () => {};
+export type User = null;
